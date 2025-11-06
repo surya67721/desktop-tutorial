c6ef972d2fbecca5894f309db1d71ca1c450896c
surya67721 <Surya.a@oppty.in>
2025-11-06 18:33:17 +0530

    task

---
+++ Exta task/script.js
@@ -0,0 +1,59 @@
+//  Using Const 
+
+// var city = "Hyderabad";
+// let age = 20;
+// const country = "India";
+
+
+//  var city = "Maharastra";
+//  age = 25;            
+//  country = "USA"; 
+
+//  age = 30;
+
+//  let village = "Aswaraopet"
+
+//  let surya = "oppty";
+
+
+// console.log(village);
+
+
+// const country = "russia";
+
+// console.log(country);
+
+
+// let city = "banglore";
+// let age = 25;
+// let country = "India";
+
+ let1 = "10"
+ let2 = "20"
+
+// console.log(city);
+// console.log(age);
+// console.log(country);
+
+
+
+   city = "vizag";
+  age = "29";
+  country = "Aus";
+
+ console.log(let1+let2);
+
+
+//var city = "hyderabad";
+//var age = "35";
+//var country = "India";
+
+//var city = "Vizag"
+//var age = "30"
+//var country = "Aus"
+
+//var country = "uk"
+
+//let village = "kothagudem" 
+
+//console.log(village);
