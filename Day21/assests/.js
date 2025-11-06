fafd22d0c4765aa2672b628726577ce499f655ba
surya67721 <Surya.a@oppty.in>
2025-11-06 10:11:54 +0530

    day21

---
+++ Day21/assests/.js
@@ -0,0 +1,13 @@
+
+<script src="./practice.js"></script>
+//variables
+let numberVar = 10;              // Number
+let stringVar = "Hello World";   // String
+let boolVar = true;              // Boolean
+let arrayVar = [1, 2, 3];        // Array
+let objectVar = {x: 1, y: 2};    // Object
+
+let myVariable = "Displayed in console!";
+console.log(myVariable);  // This will show the value in the browser's console
+let string ="hello world";
+console.log(person);
