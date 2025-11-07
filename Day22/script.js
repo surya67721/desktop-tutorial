c0d6a116b877abf16c86d248ea6bb23e5aff74ff
surya67721 <Surya.a@oppty.in>
2025-11-07 18:46:43 +0530

    Day22

---
+++ Day22/script.js
@@ -0,0 +1,19 @@
+  let item1 = 8;
+let item2 = 2;
+let total = item1 + item2;
+const discount = total/100*30;
+let finalPrice = total - discount;
+console.log("FinalPrice:", finalPrice);
+console.log(discount);
+console.log(item2);
+
+let Country = "India"; // Input country variable
+let Age = 10;          // Input age variable
+
+
+if (Age >= 18 && (Country == "India" || Country == "USA")) {
+    console.log("Allowed");
+} else {
+    console.log("not Allowed");
+} // Make sure all curly braces match!
+console.log(country);
